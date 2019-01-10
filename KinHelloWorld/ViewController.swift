98a14542e9b755bd6b9a098e4fccd96ae3c6bb97
Agathe Battestini <abatu@allstate.com>
2019-01-10 08:57:16 -0800

    Initial Commit

---
+++ KinHelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KinHelloWorld
+//
+//  Created by Agathe Battestini on 1/10/19.
+//  Copyright © 2019 Kin Ecosystem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
